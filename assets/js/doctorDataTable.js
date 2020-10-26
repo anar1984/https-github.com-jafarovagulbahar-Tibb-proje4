8f1b4d6000fc948b9bbe3c45afae78193d768c9d
unknown <jafarova2020@gmail.com>
2020-10-26 12:54:46 +0400

    new3

--- assets/js/doctorDataTable.js
+++ assets/js/doctorDataTable.js
@@ -38,12 +38,9 @@ var id= Math.floor(Math.random()*9999999);
         .append($('<a>').addClass('dropdown-item').attr('href','#').attr('data-toggle','modal').attr('data-target','#myModal26').append('Müalicə (Konservativ)'))
         .append($('<a>').addClass('dropdown-item').attr('href','#').attr('data-toggle','modal').attr('data-target','#myModal27').append('Müalicə (Səs terapiyası)'))
         .append($('<a>').addClass('dropdown-item').attr('href','#').attr('data-toggle','modal').attr('data-target','#myModal28').append('Müalicə (Cərrahi)'))
-        
-        
+           
         
         )  )
-
-
 
 
         .append($('<td>').append('1'))
@@ -65,3 +62,95 @@ var id= Math.floor(Math.random()*9999999);
     	)
       table.append(t); 
 }
+
+
+
+function PatientQuestion(){
+
+     var patient=$('#patinetAddModalBody');
+     var p=$('<div>').addClass('row')
+     
+         .append($('<div>').addClass('form-group col-md-4').append($('<label>').append('Patient ID'))
+         .append($('<input>').addClass('form-control  apd-form-input').attr('type','text').attr('id','patientName')))
+          
+    
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Səsdə Yorulma'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+    
+    
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Səsdə İncələmə'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+    
+    
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Səsdə Gərginlik'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+     
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Udqunmanın Çətinləşməsi'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+    
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Boğazda ağrı, batma hissi'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+      
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Səs batması'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Səsdə qalınlaşma'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Səsdə boğuqluq'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+    
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Səsdə Kobudluq'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+    
+        .append($('<div>').addClass('form-group col-md-4 patientSelectBox').append($('<label>').append('Tənginəfəslik'))
+        .append($('<select>').addClass('noSearch selectStyle')
+        .append($('<option>').addClass('active').append(''))
+        .append($('<option>').append('Xeyir'))
+        .append($('<option>').append('Beli'))
+        .append($('<option>').append('---')) ) )
+    
+        .append($('<div>').addClass('form-group col-md-4').append($('<label>').append('İzahat'))
+        .append($('<input>').addClass('form-control  apd-form-input').attr('type','text').attr('id','')))
+         
+    
+    
+        patient.append(p); 
+     }
+    
